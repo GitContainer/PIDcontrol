957d101310477ee98a0381b0e791adfef2cc256f
TIAN <tianzw06@gmail.com>
2017-12-24 14:37:03 +0800

    new parameters

--- core/main.py
+++ core/main.py
@@ -4,11 +4,11 @@ from motor import Motor
 import numpy as np
 
 A = np.zeros((3,4))
-A[0,:] = np.array([1e-4, 0, -0, 0])
+A[0,:] = np.array([2e-4, 0, -0, 0])
 A[1,:] = np.array([0, 0, 0, 0])
-A[2,:] = np.array([1, 0, 0, 0])
+A[2,:] = np.array([8, 0, 0, 0])
 
-motor = Motor(inertia=0.0025, damping=0)
+motor = Motor(inertia=0.0025, damping=0.0001)
 motor.setPIDParameters(A)
 motor.run()
 
